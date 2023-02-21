2adad9f2c9b364f82770ecf9d7fd502f9360c5d5
jaworskiwojciech <jaworski.wojciech94@gmail.com>
2023-02-21 20:42:13 +0100

    tasks 8 and 9 done

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,19 @@
+const loginForm = document.querySelector(".login-form");
 
+function checkSubmit(event) {
+  event.preventDefault();
+
+  const {
+    elements: { email, password },
+  } = event.currentTarget;
+
+  if (email.value === "" || password.value === "") {
+    return alert("Proszę wypełnić wszystkie pola!");
+  }
+
+  const result = { email: email.value, password: password.value };
+  console.log(result);
+  event.currentTarget.reset();
+}
+
+loginForm.addEventListener("submit", checkSubmit);
--- js/task-09.js
+++ js/task-09.js
@@ -3,3 +3,15 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+const bodyEl = document.querySelector("body");
+
+const changeColor = document.querySelector(".change-color");
+
+const colorName = document.querySelector(".color");
+
+changeColor.addEventListener("click", changeBgColor);
+
+function changeBgColor() {
+  bodyEl.style.backgroundColor = `${getRandomHexColor()}`;
+  colorName.textContent = getRandomHexColor();
+}
